d7802a5bd8becb825bcf8d46b28dc4ca46f95615
DennisMuasyaWambua <muasyathegreat4@gmail.com>
2023-12-17 19:33:27 +0300

    json response

--- payments/views.py
+++ payments/views.py
@@ -185,7 +185,7 @@ class CallBackUrl(APIView):
         logger = logging.getLogger('django.server')
         json_response = json.dumps(data)
         logger.info(json_response)
-        logger.info(json_response.keys())
+        logger.info(json_response['stkCallback'])
         
         # response_code = json_response["Body"]["stkCallback"]["ResultCode"]
         # logger.info("Result code"+json_response["ResultCode"])
