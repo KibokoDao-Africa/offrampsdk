4f76d8b268ec93ba0c11303c361150fee2d65a19
timothyAgevi <timothyagevi@gmail.com>
2024-03-08 02:50:08 +0300

    routes form onramp

---
+++ src/routes/onramp.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import * as controller from '../controllers/onramp';
+
+const router = Router();
+
+// CRUD Routes /onramptx
+router.get('/', controller.getOnRampTx); // /onramptx
+router.get('/:onRampTxId', controller.getOnRampTxById); // /onramptx/:onRampTxId
+router.post('/', controller.createOnRampTx); // /onramptx
+router.put('/:onRampTxId', controller.updateOnRampTx); // /onramptx/:onRampTxId
+
+export default router;
