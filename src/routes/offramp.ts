0e00f6b04bd80b60a26d2cf607c4133586106bf5
timothyAgevi <timothyagevi@gmail.com>
2024-03-08 02:52:07 +0300

    routes form offramp

---
+++ src/routes/offramp.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import * as controller from '../controllers/offramp';
+
+const router = Router();
+
+// CRUD Routes /offramptx
+router.get('/', controller.getOfframpTx); // /offramptx
+router.get('/:offrampTxId', controller.getOfframpTxById); // /offramptx/:offrampTxId
+router.post('/', controller.createOfframpTx); // /offramptx
+router.put('/:offrampTxId', controller.updateOfframpTx); // /offramptx/:offrampTxId
+
+export default router;
