72b83882f5a4000c06bcacdcefd1da3589683312
timothyAgevi <timothyagevi@gmail.com>
2024-03-09 01:13:16 +0300

    update on routes and controllers

--- dist/util/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize(process.env.PG_DB, process.env.PG_USER, process.env.PG_PASSWORD, {
-    host: process.env.PG_HOST,
-    dialect: 'postgres',
-});
-exports.default = sequelize;
-//# sourceMappingURL=database.js.map
